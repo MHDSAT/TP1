aade8518ee42c26b98f8ceb8c369a99c6f792e00
Mohamed GAYE <85800189+MHDSAT@users.noreply.github.com>
2021-06-21 11:32:38 +0000

    Update exo14.c

--- exo14.c
+++ exo14.c
@@ -1,21 +1,17 @@
 #include<stdio.h>
 #include<math.h>
 int main(){
-float pi(int precision){
-    float last_a=1,new_a,last_b=1/sqrt(2),new_b,ret,sigma=0,p=2;
-    for(int i=1;i<=precision;i++){
-        new_a = (last_a + last_b)/2;
-        new_b = sqrt(last_a*last_b);
-        sigma += p*(new_a*new_a - new_b*new_b);
-        last_a = new_a; last_b = new_b;p *= 2;
-    }
-    return (4 * new_a * new_a)/(1 - 2 * sigma);
-}
-int main(){
-    int n;
+    int n,i;
     printf("Donnez n ");
     scanf("%d",&n);
-    printf("Pi = %f\n",pi(n));
+    float last_a=1,new_a,last_b=1/sqrt(2),new_b,sigma=0,p=2,pi;
+    for(i=1;i<=n;i++){
+        new_a = (last_a + last_b)/2;
+        new_b = sqrt(last_a*last_b);
+        sigma = sigma + p*(new_a*new_a - new_b*new_b);
+        last_a = new_a; last_b = new_b;p = p * 2;
     }
+    pi=(4 * new_a * new_a)/(1 - 2 * sigma);
+    printf("Pi = %f\n",pi);
    return 0;
   }
