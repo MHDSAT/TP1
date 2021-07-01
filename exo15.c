2c36c56fba89cd24b7d33e182a78d0fb586ae81d
Mohamed GAYE <85800189+MHDSAT@users.noreply.github.com>
2021-07-01 14:10:31 +0000

    Update exo15.c

--- exo15.c
+++ exo15.c
@@ -1,32 +1,22 @@
 #include<stdio.h>
 int main(){
     float tab1[100],tab2[100];
-    int taille1,taille2=0,end;
-    printf("Taille ? ");
-    scanf("%d",&taille1);
-    end=taille1;
-    for(int i=0;i<taille1;i++){
-        printf("Element %d ",i);
-        scanf("%f",&tab1[i]);
+    int taille,i=0;
+    do{
+        printf("Taille ? ");
+        scanf("%d",&taille);
+    }while(taille<0);
+    for(int ind=0;ind<taille;ind=ind+2){
+        tab2[i]=tab1[ind];
+        i++;
     }
-    for(int i=0;i<taille1;i++){
-        if((int)tab1[i]%2==0)
-            tab2[taille2++]=tab1[i];
-        else
-            tab1[end++]=tab1[i];
-        i--;
+    for(int ind=1;ind<taille;ind=ind+2){
+        tab2[i]=tab1[ind];
+        i++;
     }
-    for(int i=0;i<end-taille1;i++){
-        tab1[i]=tab1[i+taille1];
-    }
-    taille1=end-taille1;
-    printf("t1\n");
-    for(int i=0;i<taille1;i++){
-        printf("%f\n",tab1[i]);
-    }
-    printf("t2\n");
-    for(int i=0;i<taille2;i++){
-        printf("%.2f\n",tab2[i]);
+    printf("tableau :\n");
+    for(i=0;i<taille;i++){
+        printf("%f\n",tab2[i]);
     }
     return 0;
 }
